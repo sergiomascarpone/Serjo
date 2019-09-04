dedf089670311a4605cb695c3530de7bf0fca136
Serjo <49415556+sergiomascarpone@users.noreply.github.com>
2019-09-04 21:24:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+'usestrict'
+
+// let a = { C:  1} ;
+// const b ={ D: 1} ;
+
+// a.C = 2;
+// b.D = 2;
+// console.log(a, b);
+
+
+// let a = 'qwerty';
+// const result = a.split('')   //разрабатывает строку на строки в подстроке
+// console.log(result);
+// console.log(a.slise(2,4));  //достает 2 и 4 буквы!!!!!!
+
+
+// let a = 'qwwrty';
+
+// let result2 = [];
+// let helpstr ='';
+
+// for (let i = 0; i < a.length; i++)  {
+//     if (a[i] !=='e')  {
+//         helpstr += a[i];
+//     }else  {
+//         result2.push(helpstr);
+//         helpstr="";
+//     }
+// }
+// if (helpstr)  {
+//     result2.push(helpstr);
+
+// }
+// const result = a.split('e');
+// console.log(result, result2);   //метод на этих строках заменяет - метод (split)!!!!
+
+// let a = false;   //если true то в консоли true,если false то в консоли  false!!!!
+// let b = true;
+
+// const result = a;
+// if (!a)  {
+//     console.log(true);
+// } else {
+//     console.log(true);
+// }
+
+// let a = [1,2,3,4,5,6,7,8,9,10]; //рабоtа с массивом через map!!!!
+
+// const result = [];
+// const changeValue = (value, index) => {
+//     return value * value;
+// }
+
+// for ( let i = 0; i < a.length; i++) 
+//  {
+//     const changeValue = changeValue(a[i], i);
+//     result.push(changeValue)
+// }
+
+// console.log(result);
+
+
+// const cat = {
+//     color: ['red', 'black'],
+//     weight: 8,
+//     name: 'Mursik',
+// }
+
+// cat.color
+
+// console.log(cat.weight);
+
+let str = 'qwerty'
+
+console.log(str.substr(2,4));
+
