5ad5fa12c1f5135536c8d28ea2819ea6cb43db97
Serjo <49415556+sergiomascarpone@users.noreply.github.com>
2019-09-04 17:33:00 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,45 +1,67 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.css'
 
-const imgArr = [
-  'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg',
+
+ class App extends Component {
+  state = {
+    images: [
+      'https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg',
   'https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
   'https://image.shutterstock.com/image-photo/colorful-flower-on-dark-tropical-260nw-721703848.jpg',
   'https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg',
-
-];
-
-
-
-class App extends Component {
-  state = {
-    imageIndex: 0,
+    ],
+    counter: 0
   };
+  
 
-  handleRightClick = () => {
-    this.setState({ imageIndex: this.state.imageIndex + 1 });
+  onRightSlide = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    })
+    if (this.state.counter === this.state.images.length - 1) {
+      this.setState({
+        counter: 0
+      })
+    }
+    // console.log(this.state.counter)      
   }
 
-  handleLeftClick = () => {
-    this.setState({ imageIndex: this.state.imageIndex - 1 });
+  onleftSlide = () => {
+    this.setState({
+      counter: this.state.counter - 1
+    })
+    if (this.state.counter === 0) {
+      this.setState({
+        counter: this.state.images.length - 1
+      })
+    }
+    // console.log(this.state.counter)      
   }
 
   render() {
     return (
-
-      <div className="App">
-        <img src={imgArr[this.state.imageIndex]} />
-
-        {this.state.imageIndex !== imgArr.length - 1 && <button onClick={this.handleRightClick}>Right</button>}
-        {this.state.imageIndex !== 0 && <button onClick={this.handleLeftClick}>Left</button>}
-
+      <div className="mainApp">
+        <img className="background" src={this.state.images[this.state.counter]} alt=''></img>
+        <button 
+        className='leftArrow' 
+        onClick={this.onleftSlide}>
+          -
+          </button>
+        <button 
+        className='rightArrow' 
+        onClick={this.onRightSlide}>
+           +
+           </button>
+        <div className="circles">
+          {
+            this.state.images.map((img, index) => (
+              <div key={img} className={index === this.state.counter ? 'blackCircle' : 'circle'}></div>
+            ))
+          }
+        </div>
       </div>
-    )
+    );
   }
 }
 
-
-
-
-
-export default App;
+export default  App;
