a57bb89b7aa5b70129b06c04ceb7dcbe93650b05
Serjo <49415556+sergiomascarpone@users.noreply.github.com>
2019-11-11 21:36:44 +0300

    11.11.2019
    Прогонка setterov,getterov,цыклы

--- index.js
+++ index.js
@@ -1,76 +1,45 @@
-'usestrict'
+const inputElements = document.querySelectorAll('[m-model]');
+const boundElements = document.querySelectorAll('[m-bind]');
+//console.log(inputElements);
+let scope = {
+    //name: 'Bruce'
+};
 
-// let a = { C:  1} ;
-// const b ={ D: 1} ;
-
-// a.C = 2;
-// b.D = 2;
-// console.log(a, b);
-
-
-// let a = 'qwerty';
-// const result = a.split('')   //разрабатывает строку на строки в подстроке
-// console.log(result);
-// console.log(a.slise(2,4));  //достает 2 и 4 буквы!!!!!!
+function init() {
+    for (let el of inputElements) {
+        if (el.type === "text") {
+            let propName = el.getAttribute('m-model');
 
 
-// let a = 'qwwrty';
+            el.addEventListener('keyup', (e) => {
+                //console.log(e)
+                scope[propName] = e.target.value;
+            });
+            setPropUpdate(propName);
+        }
+    }
+}
 
-// let result2 = [];
-// let helpstr ='';
+function setPropUpdate(prop) {
+    let value;
 
-// for (let i = 0; i < a.length; i++)  {
-//     if (a[i] !=='e')  {
-//         helpstr += a[i];
-//     }else  {
-//         result2.push(helpstr);
-//         helpstr="";
-//     }
-// }
-// if (helpstr)  {
-//     result2.push(helpstr);
+    Object.defineProperty(scope, prop, {
 
-// }
-// const result = a.split('e');
-// console.log(result, result2);   //метод на этих строках заменяет - метод (split)!!!!
+        set: (newValue) => {
+            value = newValue;
+            // console.log(newValue);
+            for (let el of inputElements) {
+                if (el.getAttribute('m-model') === prop) {
+                    el.value = newValue;
+                }
+            }
+        },
 
-// let a = false;   //если true то в консоли true,если false то в консоли  false!!!!
-// let b = true;
+        get: () => {
+            return value;
+        },
+        // enumerable: true
+    });
+};
 
-// const result = a;
-// if (!a)  {
-//     console.log(true);
-// } else {
-//     console.log(true);
-// }
-
-// let a = [1,2,3,4,5,6,7,8,9,10]; //рабоtа с массивом через map!!!!
-
-// const result = [];
-// const changeValue = (value, index) => {
-//     return value * value;
-// }
-
-// for ( let i = 0; i < a.length; i++) 
-//  {
-//     const changeValue = changeValue(a[i], i);
-//     result.push(changeValue)
-// }
-
-// console.log(result);
-
-
-// const cat = {
-//     color: ['red', 'black'],
-//     weight: 8,
-//     name: 'Mursik',
-// }
-
-// cat.color
-
-// console.log(cat.weight);
-
-let str = 'qwerty'
-
-console.log(str.substr(2,4));
-
+init();
